006712f7f508f0c42c87baaf4150b23aeac70a73
nguyenminhthiet <thietnm116204@gmail.com>
2026-07-24 21:09:12 +0700

    HealthController

---
+++ back-end/e-commerce/src/main/java/com/example/e_commerce/config/heal/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.e_commerce.config.heal;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("OK");
+    }
+}
