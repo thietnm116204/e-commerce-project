17f457558569e0c6a2d21057daa63d6400c57ab0
nguyenminhthiet <thietnm116204@gmail.com>
2026-07-25 20:12:40 +0700

    fe kết nối sever

--- font-end/e-commerce/vite.config.js
+++ font-end/e-commerce/vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const local = 'http://localhost:8080'
+const production = 'https://e-commerce-project-0hsd.onrender.com'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://e-commerce-project-0hsd.onrender.com',
+        target: production,
         changeOrigin: true,
       },
     },
